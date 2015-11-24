43178b5f7e66f98c1e3d2caef0582a9f5df1205e
Charlotte Kwan <kwan.charlotte.cl@gmail.com>
2015-11-25 00:19:59 +0800

    List API added (passing in gift id WIP)

--- app/controllers/api/api.js
+++ app/controllers/api/api.js
@@ -4,11 +4,11 @@ var mongoose = require('mongoose');
 var Gift = require('../../models/gift');
 var List = require('../../models/list');
 
-module.exports = function (app) {
+module.exports = function (app){
   app.use('/', router);
 };
 
-function authenticatedUser(req, res, next) {
+function authenticatedUser(req, res, next){
   // If the user is authenticated, then we continue the execution
   // Otherwise the request is always redirected to the home page
   if (req.isAuthenticated()) {
@@ -23,11 +23,11 @@ router.get('/secret', authenticatedUser, function (req, res, next) {
 });
 
 // Gift-index
-router.get('/api/gifts', function(req, res, next) {
+router.get('/api/gifts', function(req, res, next){
   Gift.find({}, function(err, gifts){
     if (err) res.json({message : err})
-    res.json({gifts : gifts});
-  })
+    res.json({gifts});
+  }).select('-createdBy')
 });
 
 //Gift-show
@@ -36,8 +36,8 @@ router.get('/api/gifts/:id', function(req, res, next) {
 
   Gift.findById(giftId, function (err,gift){
     if (err) res.json({message : err})
-    res.json({gift : gift});
-  })
+    res.json({gift});
+  }).select('-createdBy')
 });
 
 // Gift-create
@@ -45,21 +45,21 @@ router.post("/api/gifts", authenticatedUser, function(req, res){
   var giftParams = req.body.gift;
   giftParams.createdBy = req.user._id;
 
-  Gift.create(giftParams, function (err, gift) {
+  Gift.create(giftParams, function (err, gift){
     if (err) res.json({message : err})
-    res.json({gift : gift})
+    res.json({gift})
   });
 })
 
 //Gift-edit
-router.put('/api/gifts/:id', authenticatedUser, function(req, res, next) {
+router.put('/api/gifts/:id', authenticatedUser, function(req, res, next){
 
   var currentUser = req.user.id;
   var giftId = req.params.id;
 
   Gift.findById(giftId, function (err , gift){
     if (err) res.json({message : err})
-    if (currentUser !=  gift.createdBy) {
+    if (currentUser !=  gift.createdBy){
       res.json({message: "You are not the creator!"});
     } else {
     if (req.body.gift.name) gift.name = req.body.gift.name;
@@ -82,10 +82,9 @@ router.delete('/api/gifts/:id', authenticatedUser, function(req, res, next){
 
   Gift.findById(giftId, function (err , gift){
     if (err) res.json({message : err})
-    if (currentUser !=  gift.createdBy) {
+    if (currentUser !=  gift.createdBy){
       res.json({message: "You are not the creator!"});
     } else {
-      // Gift.findByIdAndRemove(giftId, function(err){
       gift.remove(function(err){
       if (err) res.json({message: err})
       res.json({message: "Gift has been removed"})
@@ -94,16 +93,45 @@ router.delete('/api/gifts/:id', authenticatedUser, function(req, res, next){
   })
 })
 
-//List-get
-// router.get('/api/mylist', function(req, res, next) {
-//   var currentUser = "";
+//List-get api/mylist
+router.get('/api/mylist', authenticatedUser, function(req, res, next){
 
-//   List.find({}, function(err, lists){
-//     if (err) res.json({message : err})
-//     res.json({lists : lists});
-//   })
-// });
+  var currentUser = req.user.id;
 
-  //List-post
+  List.find({user: currentUser}, function(err, lists){
+    if (err) res.json({message : err})
+    res.json({lists : lists});
+  })
+});
 
-  //List-delete
+//List-post api/mylist
+router.post('/api/mylist', authenticatedUser, function(req, res){
+
+  var listParams = req.body.list;
+  listParams.user = req.user._id;
+  listParams.gift = req.params.id;
+
+  List.create(listParams, function(err, list){
+    if (err) res.json({message : err})
+    res.json({list : list});
+  })
+});
+
+//List-delete api/mylist/:id
+router.delete('/api/mylist/:id', authenticatedUser, function(req, res, next){
+
+  var currentUser = req.user.id;
+  var listId = req.params.id;
+
+  List.findById(listId, function (err , list){
+    if (err) res.json({message : err})
+    if (currentUser !=  list.user){
+      res.json({message: "You are not authorized"});
+    } else {
+      list.remove(function(err){
+      if (err) res.json({message: err})
+      res.json({message: "Favourite has been removed"})
+      });
+    }
+  })
+})
--- app/models/list.js
+++ app/models/list.js
@@ -5,7 +5,9 @@ var Schema   = mongoose.Schema;
 
 var ListSchema = new Schema({
   user: { type: Schema.Types.ObjectId, ref: 'User' },
-  gift: { type: Schema.Types.ObjectId, ref: 'Gift' }
+  gift: { type: Schema.Types.ObjectId, ref: 'Gift' },
+  recipients: [String],
+  quantity: Number
 });
 
 module.exports = mongoose.model('List', ListSchema);
